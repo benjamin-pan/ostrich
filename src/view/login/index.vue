dd46d4993e1625b27b83c825a283f7a84cbbc887
benjamin <1010629840@qq.com>
2018-07-28 09:41:41 +0800

    Environment set up

---
+++ src/view/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    这是登录页
+  </div>
+</template>
+<style lang="scss" scoped>
+  @import './index.scss';
+</style>
+<script>
+export default {
+//  name: 'Login',
+  data () {
+    return {}
+  },
+  components: {},
+  created () {
+  },
+  mounted () {
+  },
+  beforeDestroy () {
+  },
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
