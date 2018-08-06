c6f8ac3fe8d09754008859899f76bb240ee33ee2
xiaobani <18407398652@163.com>
2018-08-06 10:15:21 +0800

    首页细节修改

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -163,59 +163,50 @@
     <div class="right content-right">
       <today-flash></today-flash>
       <headline-column></headline-column>
-      <div class="hotnews marginb-30">
-        <div class="tit">
-          <span style="border-radius: 25%; background: #CD3333; height: 20px; width: 20px; margin-left: 15px;">
-            <img style="margin-top: -3px; margin-left: 2px;" src="../../assets/images/hot.png">
-          </span>
-          <span>热门新闻</span>
+        <div class="hotnews marginb-30">
+        <div class="tit news24">
+          <img src="../../assets/svg/二级页-24小时新闻icom.svg">
+          <span>24小时新闻</span>
         </div>
         <div class="con bgc paddingclearfix">
-          <div class="con bgc  clearfix">
+          <div class="con bgc clearfix">
             <ul class="clearfix tuoniao-hot-news">
-              <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
-                <img class="left" src="../../assets/images/2018071811084089.jpeg" alt="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
-              </a>
-              <ol class="right">
-                <span class="tuoniao-hot-content">
-                  <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来</a>
-                </span>
-                <li class="font12 gray8">2018-07-18</li>
-                <li class="right">
-                  <img style="height: 20px;width: 15px;margin-left: 100px;" src="../../assets/images/eye.png">
-                  <a href="javascript:;" style="margin-left: 12px;" data-id="43710" data-src="" class="generate-share-image right">3098</a>
-                </li>
-              </ol>
+              <li class="news-li">
+                <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
+                  <img class="left" src="../../assets/images/2018071811084089.jpeg" alt="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
+                </a>
+                <ol class="right">
+                  <li>
+                    <span class="tuoniao-hot-content">
+                      <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来</a>
+                    </span>
+                  </li>
+                  <li class="font12 gray8 li-time">12分钟前</li>
+                  <li class="right newsTraffic">
+                    <img src="../../assets/svg/首页新闻-浏览量icon.svg">
+                    <a href="javascript:;" data-id="43710" data-src="" class="generate-share-image right">3098</a>
+                  </li>
+                </ol>
+              </li>
             </ul>
             <ul class="clearfix tuoniao-hot-news">
-              <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
-                <img class="left" src="../../assets/images/2018071811084089.jpeg" alt="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
-              </a>
-              <ol class="right">
-                <span class="tuoniao-hot-content">
-                  <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来</a>
-                </span>
-                <li class="font12 gray8">2018-07-18</li>
-                <li class="right">
-                  <img style="height: 20px;width: 15px;margin-left: 100px;" src="../../assets/images/eye.png">
-                  <a href="javascript:;" style="margin-left: 12px;" data-id="43710" data-src="" class="generate-share-image right">3098</a>
-                </li>
-              </ol>
-            </ul>
-            <ul class="clearfix tuoniao-hot-news">
-              <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
-                <img class="left" src="../../assets/images/2018071811084089.jpeg" alt="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
-              </a>
-              <ol class="right">
-                <span class="tuoniao-hot-content">
-                  <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来</a>
-                </span>
-                <li class="font12 gray8">2018-07-18</li>
-                <li class="right">
-                  <img style="height: 20px;width: 15px;margin-left: 100px;" src="../../assets/images/eye.png">
-                  <a href="javascript:;" style="margin-left: 12px;" data-id="43710" data-src="" class="generate-share-image right">3098</a>
-                </li>
-              </ol>
+              <li class="news-li">
+                <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
+                  <img class="left" src="../../assets/images/2018071811084089.jpeg" alt="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">
+                </a>
+                <ol class="right">
+                  <li>
+                    <span class="tuoniao-hot-content">
+                      <a href="https://www.tuoniaox.com/43416.html" target="_blank" title="加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来">加拿大Rivemont加密货币基金：目前加密货币市场并非牛市，但非常看好比特币的未来</a>
+                    </span>
+                  </li>
+                  <li class="font12 gray8 li-time">12分钟前</li>
+                  <li class="right newsTraffic">
+                    <img src="../../assets/svg/首页新闻-浏览量icon.svg">
+                    <a href="javascript:;" data-id="43710" data-src="" class="generate-share-image right">3098</a>
+                  </li>
+                </ol>
+              </li>
             </ul>
           </div>
         </div>
